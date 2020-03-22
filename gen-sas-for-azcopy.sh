3ad8f99325f2ec60f5066298ef64426493761441
Herb Stahl <ghstahl@gmail.com>
2020-03-22 10:00:10 -0700

    bump

--- gen-sas-for-azcopy.sh
+++ gen-sas-for-azcopy.sh
@@ -1,8 +1,21 @@
+die () {
+    echo >&2 "$@"
+    echo "$ ./gen-sas-for-azcopy.sh [storage-account-name] [container-name]"
+    exit 1
+}
+REQUIRED_ARGS=2
+[ "$#" -eq $REQUIRED_ARGS ] || die "$REQUIRED_ARGS argument required, $# provided"
+
+# ACCOUNT="stsecureupload"
+# CONTAINER="intake"
+
+ACCOUNT=$1
+CONTAINER=$2
+
 EXPIRE=$(date -u -d "1 day" '+%Y-%m-%dT%H:%M:%SZ')
 START=$(date -u -d "-1 day" '+%Y-%m-%dT%H:%M:%SZ')
  
-ACCOUNT="stsecureupload"
-CONTAINER="intake"
+
 SAS=$(az storage container generate-sas --account-name "$ACCOUNT" --name "$CONTAINER" --start "$START" --expiry "$EXPIRE" --https-only --permissions acdlrw --auth-mode login --as-user)
 
 SAS=$(sed -e 's/^"//' -e 's/"$//' <<<"$SAS")
